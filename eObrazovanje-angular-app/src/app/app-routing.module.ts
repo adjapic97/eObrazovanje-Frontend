c684acc83b0db8854bdc6797b4636a5b524f6c54
BCharles911 <djapic.andrej@gmail.com>
2020-08-03 00:23:24 +0200

    installed nebular eva-icons pack

--- eObrazovanje-angular-app/src/app/app-routing.module.ts
+++ eObrazovanje-angular-app/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { NbThemeModule, NbMenuService, NbMenuModule, NbSidebarService } from '@nebular/theme';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes),
+  NbMenuModule.forRoot(),
+  NbThemeModule.forRoot()
+  ],
+  exports: [RouterModule],
+  providers: [NbSidebarService]
 })
 export class AppRoutingModule { }
--- eObrazovanje-angular-app/src/app/app.module.ts
+++ eObrazovanje-angular-app/src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NbSidebarModule, NbLayoutModule, NbButtonModule, NbLayoutComponent } from '@nebular/theme';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    NbLayoutModule,
+    NbSidebarModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
