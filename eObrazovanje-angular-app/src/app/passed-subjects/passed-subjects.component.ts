f2e0dfc91fb1d0a46934368919a0c756a0dea3fa
Milos Stjepanovic <stjepke92@gmail.com>
2020-11-22 18:46:19 +0100

    uradjena stranica za polozene predmete

--- eObrazovanje-angular-app/src/app/passed-subjects/passed-subjects.component.ts
+++ eObrazovanje-angular-app/src/app/passed-subjects/passed-subjects.component.ts
@@ -5,8 +5,10 @@ interface Subject {
   oznaka: string;
   naziv_predmeta: string;
   iznos: number;
+  ects: number;
   datum: Date;
-  nastavnik: string;
+  nastavnik: string
+  ocena: number;
 }
 
 const SUBJECTS: Subject[] = [
@@ -14,36 +16,46 @@ const SUBJECTS: Subject[] = [
     oznaka: 'M1',
     naziv_predmeta: 'Matematika 1',
     iznos: 300.00,
+    ects: 8,
     datum: new Date("2020-01-18"),
-    nastavnik: 'Mirko Mirkovic'
+    nastavnik: 'Mirko Mirkovic',
+    ocena: 7
   },
   {
     oznaka: 'EO',
     naziv_predmeta: 'EObrazovanje',
     iznos: 200.00,
+    ects: 6,
     datum: new Date("2020-01-16"),
-    nastavnik: 'Mirko Mirkovic'
+    nastavnik: 'Nikola Nikolic',
+    ocena: 8
   },
   {
-    oznaka: 'EO',
-    naziv_predmeta: 'EObrazovanje',
+    oznaka: 'EUP',
+    naziv_predmeta: 'eUprava',
     iznos: 200.00,
-    datum: new Date("2020-01-16"),
-    nastavnik: 'Mirko Mirkovic'
+    ects: 3,
+    datum: new Date("2020-01-13"),
+    nastavnik: 'Nikola Nikolic',
+    ocena: 6
   },
   {
-    oznaka: 'EO',
-    naziv_predmeta: 'EObrazovanje',
+    oznaka: 'UD',
+    naziv_predmeta: 'Upravljanje datotekama',
     iznos: 200.00,
-    datum: new Date("2020-01-16"),
-    nastavnik: 'Mirko Mirkovic'
+    ects: 7,
+    datum: new Date("2020-02-16"),
+    nastavnik: 'Mirko Mirkovic',
+    ocena: 9
   },
   {
-    oznaka: 'EO',
-    naziv_predmeta: 'EObrazovanje',
+    oznaka: 'ENG1',
+    naziv_predmeta: 'Engleski 1',
     iznos: 200.00,
-    datum: new Date("2020-01-16"),
-    nastavnik: 'Mirko Mirkovic'
+    ects: 5,
+    datum: new Date("2020-03-21"),
+    nastavnik: 'Mirko Mirkovic',
+    ocena: 8
   },
 
 
