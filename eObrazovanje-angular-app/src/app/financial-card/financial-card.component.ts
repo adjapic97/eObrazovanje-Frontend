ba8622ea51c68420c21bcad5cabed9810ccbe4c6
Milos Stjepanovic <stjepke92@gmail.com>
2020-11-27 20:40:55 +0100

    finansijska kartica ende

--- eObrazovanje-angular-app/src/app/financial-card/financial-card.component.ts
+++ eObrazovanje-angular-app/src/app/financial-card/financial-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FinancialCard } from '../classes/FinancialCard';
+import { Transaction } from '../classes/Transaction';
+import { FinancialCardService } from '../services/financial-card/financial-card.service';
 
 @Component({
   selector: 'app-financial-card',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinancialCardComponent implements OnInit {
 
-  constructor() { }
+  financialCard: FinancialCard;
+  transactionDTO: Transaction[] = [];
+
+  constructor(private financialCardService: FinancialCardService) { }
 
   ngOnInit(): void {
+
+    this.financialCardService.getFinancialCardForStudent().subscribe(
+      response => this.handleFinancialCardForStudent(response)
+    )
   }
+    handleFinancialCardForStudent(response) {
+      this.financialCard = response;
+      this.transactionDTO = response.transactionDTO;
+      console.log(this.financialCard)
+    }
+
 
 }
---
+++ eObrazovanje-angular-app/src/app/services/financial-card/financial-card.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TelephoneMinus } from 'ngx-bootstrap-icons';
+import { Observable } from 'rxjs';
+import { FinancialCard } from 'src/app/classes/FinancialCard';
+
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinancialCardService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getFinancialCardForStudent(): Observable<FinancialCard[]> {
+    return this.http.get<FinancialCard[]>('http://localhost:8080/api/student/get-financial-card?studentId=' + JSON.parse(sessionStorage.getItem('auth-user')).id)
+  }
+
+}
